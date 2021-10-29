f5f6522f7815f225fa08e8dda11e6b4b9748f249
Akshit Arora <akshitarora9211@gmail.com>
2021-10-29 20:22:41 +0530

    added chakra provider and colormode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import "./App.css";
+import { Button } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Flex, Heading } from "@chakra-ui/layout";
+// import "./App.css";
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h3>Listen and Forgot</h3>
-      </header>
-    </div>
+    <Flex justify='center'>
+      <Button
+        position='absolute'
+        right='1'
+        top='1'
+        _focus={{ outline: "none" }}
+        onClick={toggleColorMode}
+      >
+        {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
+      <Heading as='h1' size='2xl'>
+        Listen and Forgot
+      </Heading>
+    </Flex>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider, extendTheme, ColorModeScript } from "@chakra-ui/react";
+
+const config = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
+// 3. extend the theme
+const theme = extendTheme({ config });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
