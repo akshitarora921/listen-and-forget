a15658564feaf5c1e801d9862588afd0ffde46a2
Nitesh Kumar <76529189+Nktech-Official@users.noreply.github.com>
2021-10-30 11:56:25 +0530

    Made Login Work With React-Router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Button } from "@chakra-ui/button";
 import { useColorMode } from "@chakra-ui/color-mode";
 import { Flex, Heading } from "@chakra-ui/layout";
 // import "./App.css";
-import SpotifyLogin from "./SpotifyLogin"
+import {SpotifyLogin} from "./SpotifyLogin"
+import { BrowserRouter as Router,Route,Switch } from "react-router-dom";
+
 
 function App() {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <>
+    <Router>
     <Flex justify='center'>
       <Button
         position='absolute'
@@ -24,8 +26,16 @@ function App() {
       </Heading>
      
     </Flex>
-     <SpotifyLogin/>
-     </>
+    <Switch>
+      <Route exact path="/">
+      <SpotifyLogin/>
+      </Route>
+      <Route path="/auth">
+      
+      </Route>
+  
+    </Switch>
+     </Router>
   );
 }
 
--- src/SpotifyLogin.js
+++ src/SpotifyLogin.js
@@ -1,17 +1,11 @@
-import { Button } from '@chakra-ui/button';
 import React from 'react'
-// 
-function SpotifyLogin() {
-    
-    return (
-        <div>
-           {console.log(process.env.REACT_APP_LOGIN)}
-            <Button  right='3' top='3' _focus={{ outline: "none" }} onClick={() => { window.open(process.env.REACT_APP_LOGIN,"_self")}}>Login</Button>
-        </div>
-    )
+import {Button} from "@chakra-ui/button"
+
+ function SpotifyLogin() {
+   return( <Button onClick={()=>{
+        window.open(process.env.REACT_APP_LOGIN,"_self")
+    }}>Login</Button>)
+   
 }
 
-
-
-
-export default SpotifyLogin
+export {SpotifyLogin}
